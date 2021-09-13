843039a6176c591502eca99530ae913e261d0798
Tevintaylor <tevintaylor_1996@yahoo.com>
2021-09-14 03:52:10 +0800

    Life-cycle Hooks

--- src/App.vue
+++ src/App.vue
@@ -3,41 +3,27 @@
     <img class="center" src="./assets/logo.png">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
-import AllFriends from './AllFriends'
-import OnlineFriends from './OnlineFriends'
-
+import Blogs from './Blogs'
 export default {
   name: 'app',
   components: {
     Navbar,
-    OnlineFriends,
-    AllFriends
+    Blogs
   },
   data () {
     return {
       title:'My first Vue app',
-       friends:[
-        {name:'Mario' , online: true},
-        {name:'Luigi' , online: false},
-        {name:'Toad' , online: true},
-        {name:'Bowser' , online: false}
-      ]
+
     }
   },
   methods: {
-    deleteFriend(payload) {
-      console.log(payload)
-      this.friends = this.friends.filter(friend => {
-          return friend.name !== payload.name
-      })
-    }
+
   }
 }
 </script>
---
+++ src/Blogs.vue
@@ -0,0 +1,49 @@
+<template>
+ <div class="blogs">
+  <h2>{{blogTitle}}</h2>
+  <button @click="changeTitle">Change Title</button>
+ </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+
+export default {
+  name: 'Blogs',
+  data() {
+    return {
+      blogTitle:'Blogs',
+    }
+  },
+  methods:{
+    changeTitle(){
+      this.blogTitle = 'Amazing Blog Site'
+    }
+  },
+  beforeCreate(){
+    alert('BeforeCeate hook')
+  },
+  created(){
+    alert('Created hook')
+  },
+  beforeUpdate(){
+    alert('BeforeUpdate hook')
+  }
+}
+</script>
+
+<style>
+h1{
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+
+.center{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 10%;
+}
+
+</style>
