1ef08e0d6d9724c10295b6366b0f487b0cc0059d
Tevintaylor <tevintaylor_1996@yahoo.com>
2021-09-14 04:31:02 +0800

    Making Requests with Axios

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -1,34 +1,35 @@
 <template>
  <div class="blogs">
   <h2>{{blogTitle}}</h2>
-  <button @click="changeTitle">Change Title</button>
+  <div v-for="post in posts" :key="post.id">
+  <h3>{{post.title}}</h3>
+  <p>{{post.body}}</p>
+  </div>
  </div>
 </template>
 
 <script>
-import Navbar from './Navbar'
+import axios from 'axios'
 
 export default {
   name: 'Blogs',
   data() {
     return {
-      blogTitle:'Blogs',
+      posts: []
     }
   },
   methods:{
-    changeTitle(){
-      this.blogTitle = 'Amazing Blog Site'
-    }
-  },
-  beforeCreate(){
-    alert('BeforeCeate hook')
+
   },
   created(){
-    alert('Created hook')
+    axios.get('https://jsonplaceholder.typicode.com/posts/')
+    .then(response => {
+      console.log(response)
+      this.posts = response.data
+    }).catch(err =>{
+      console.log(err)
+    })
   },
-  beforeUpdate(){
-    alert('BeforeUpdate hook')
-  }
 }
 </script>
 
