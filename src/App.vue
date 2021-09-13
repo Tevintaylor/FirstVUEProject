a4c305d131a1f519b235d1f0364aa2852077986f
Tevintaylor <tevintaylor_1996@yahoo.com>
2021-09-13 08:56:30 +0800

    Passing Data with Props

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,32 @@
     <img src="./assets/logo.png">
     <h1>{{ title }}</h1>
     <Navbar />
+    <AllFriends :friends="friends" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+
 export default {
   name: 'app',
   components: {
-    Navbar
+    Navbar,
+    OnlineFriends,
+    AllFriends
   },
   data () {
     return {
-      title:'My first Vue app'
+      title:'My first Vue app',
+       friends:[
+        {name:'Mario' , online: true},
+        {name:'Luigi' , online: false},
+        {name:'Toad' , online: true},
+        {name:'Bowser' , online: false}
+      ]
     }
   }
 }
--- src/Navbar.vue
+++ src/Navbar.vue
@@ -26,7 +26,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 nav{
   text-align: center;
 }
